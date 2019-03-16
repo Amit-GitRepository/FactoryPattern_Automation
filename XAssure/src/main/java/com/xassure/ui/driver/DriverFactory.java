22a0cf62ed3c285a60d04b5e427e881e2c0dbdbd
Amit <Amit_Kumar-Kumar@external.mckinsey.com>
2019-03-16 05:43:14 +0530

    Added reporting and firefox support

--- XAssure/src/main/java/com/xassure/ui/driver/DriverFactory.java
+++ XAssure/src/main/java/com/xassure/ui/driver/DriverFactory.java
@@ -5,7 +5,7 @@ public class DriverFactory {
 	public IDriver getDriver(String typeOfDriver) {
 		if(typeOfDriver.equalsIgnoreCase("local")) 
 			return new LocalDriver();
-		else if(typeOfDriver.equalsIgnoreCase("remote")) 
+		else if(typeOfDriver.equalsIgnoreCase("remote"))
 			return new RemoteDriver();
 
 		return null;
--- XAssure/src/main/java/com/xassure/ui/tests/TestBeachbodyWorkflow.java
+++ XAssure/src/main/java/com/xassure/ui/tests/TestBeachbodyWorkflow.java
@@ -1,12 +1,18 @@
 package com.xassure.ui.tests;
 
+import java.lang.reflect.Method;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.LogStatus;
 import com.xassure.api.models.users.UsersParent;
 import com.xassure.api.services.UsersService;
+import com.xassure.reporting.Reporting;
 import com.xassure.ui.pages.CheckoutPage;
 import com.xassure.ui.pages.HomePage;
 import com.xassure.ui.pages.ProductsPage;
@@ -24,7 +30,10 @@ public class TestBeachbodyWorkflow extends BaseTest{
 	public void setUpTest() {
 		setDriver();
 		home = new HomePage(driver);
+		//Create reports directory
+		Reporting.createReportDirectory();
 		
+		//Fetch test data from users API
 		UsersService users = new UsersService();
 		UsersParent userData = users.retrieveParentAccount("https://jsonplaceholder.typicode.com/users/1");
 		firstName = userData.getName();
@@ -37,38 +46,58 @@ public class TestBeachbodyWorkflow extends BaseTest{
 		Telephone = userData.getPhone();
 	}
 	
+	@BeforeMethod
+	private void beforeMethod(Method methodName) {
+		Reporting.getLogger(methodName.getName());
+		
+	}
+	
 	@Test 
 	public void openBeachbodyApplication() {
+		Reporting.getLogger().log(LogStatus.INFO, "User has navigated to beachbody website");
 		home.navigateToUrl(ConfigPropertyReader.getProperty("baseUrl"));
 		home.findYourProgram();
+		Reporting.getLogger().log(LogStatus.INFO, "User is looking for a program");
 		productsPage = new ProductsPage(driver);
 		productsPage.clickOnAddToCart();
+		Reporting.getLogger().log(LogStatus.PASS, "User successfully added Double Profuct to Cart");
 	}
 	
 	@Test(dependsOnMethods="openBeachbodyApplication")
 	public void enterCheckoutDetails() {
+		Reporting.getLogger().log(LogStatus.INFO, "User is entering checkoutdetails");
 		checkoutPage = new CheckoutPage(driver);
 		checkoutPage.enterEmailAdress("testUser@test.com");
 		checkoutPage.enterShippingDetails(firstName, lastName, StreetAddress, OptionalAddress, City, State, PostalCode, Telephone);
+		Reporting.getLogger().log(LogStatus.PASS, "User has successfully entered email and shipping details");
 	}
 	
-	@Test(priority = 1)
+	@Test
 	public void testPass() {
 		Assert.assertTrue(true);
+		Reporting.getLogger().log(LogStatus.PASS, "ASSERTION PASSED: This is dummy pass test message template");
 	}
 	
-	@Test(priority = 2)
+	@Test
 	public void testFail() {
+		Reporting.getLogger().log(LogStatus.FAIL, "ASSERTION FAILED: This is dummy fail test message template");
 		Assert.fail();
 	}
 	
 	@Test(dependsOnMethods="testFail")
 	public void testSkip() {
+		Reporting.getLogger().log(LogStatus.SKIP, "ASSERTION SKIPPED: This is dummy skipped test message template");
 		System.out.println("Skipped test");
+	}
+	
+	@AfterMethod
+	private void afterMethod(Method methodName) {
+		Reporting.generateTestReport(methodName.getName());
 	}
 	
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
+		Reporting.generateReport();
 	}
 }
